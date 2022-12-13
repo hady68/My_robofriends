bc0c2ed66c2bd2f580429237ae82c6547e43f163
Hardikk Kamboj <kambojpushkal@gmail.com>
2022-12-13 21:53:18 +0530

    Final folder changes and Files moved

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React,{Component} from "react";
-import CardList from "./CardList";
-import Searchbox from "./Searchbox";
+import CardList from "../components/CardList";
+import Searchbox from "../components/Searchbox";
 import './App.css';
-
+import Scroll from "../components/Scroll";
 
 class App extends Component {
     constructor() {
@@ -24,11 +24,12 @@ class App extends Component {
         
     }
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         }
         )
-        if (this.state.robots.length === 0) {
+        if (robots.length === 0) {
             return <h1>Loading</h1>
         } 
         else {
@@ -36,7 +37,10 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className="f1">RoboFriends</h1>
                     <Searchbox searchChange={this.onSearchChange} />
-                    <CardList robots={filteredRobots} />
+                    <Scroll>
+                           <CardList robots={filteredRobots} />
+                    </Scroll>
+                 
                 </div>
                
             );
