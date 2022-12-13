98f65926c5e4b2c49250b1f5f2d36b6bd77ebe35
Hardikk Kamboj <kambojpushkal@gmail.com>
2022-12-13 11:47:55 +0530

    Added files

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const card = ({ name ,email , id }) => {
+    return (
+        <div className='tc bg-light-green dib ba3 pa3 ma1 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}?200x200&set=set2`} alt='robots' />
+            <div>
+                <h2> {name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+
+}
+
+export default card; 
