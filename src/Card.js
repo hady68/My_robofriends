a690500bd9e935845e33477a1691823e525cc53a
Hardikk Kamboj <kambojpushkal@gmail.com>
2022-12-13 11:57:06 +0530

    changed the card color

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const card = ({ name ,email , id }) => {
     return (
-        <div className='tc bg-light-green dib ba3 pa3 ma1 grow bw2 shadow-5'>
+        <div className='tc bg-light-blue dib ba3 pa3 ma1 grow bw2 shadow-5'>
             <img src={`https://robohash.org/${id}?200x200&set=set2`} alt='robots' />
             <div>
                 <h2> {name}</h2>
